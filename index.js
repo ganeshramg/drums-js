0a5bdacc5a02e3b9abf854112ff677d424042c8a
Ganesh Ram Gururajan <42694700+ganeshramg@users.noreply.github.com>
2021-11-05 14:41:02 +0530

    keyboardEventCommit

--- index.js
+++ index.js
@@ -7,3 +7,19 @@ function clicked(id){
     console.log(sound_file)
     play_sound(sound_file)
 }
+
+var key_sound_mapper = {
+    'a':'sounds/kick.mp3',
+    's':'sounds/snare.mp3',
+    'd':'sounds/hihat.mp3',
+    'f':'sounds/floor-tom.mp3',
+    'g':'sounds/low-tom.mp3',
+    'h':'sounds/hi-tom.mp3',
+    'j':'sounds/c1.mp3',
+    'k':'sounds/c2.mp3',
+    'l':'sounds/ride.mp3'
+}
+
+document.addEventListener('keypress',function(event){
+    play_sound(key_sound_mapper[event.key])
+})
